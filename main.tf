e6b4903d284e3c6f89fda9db3794cc542077a869
Ondairos <ondairos@gmail.com>
2022-10-01 11:56:08 +0300

    configuring main route table

--- main.tf
+++ main.tf
@@ -36,19 +36,20 @@ resource "aws_internet_gateway" "myapp-igw" {
 
 }
 
-
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
-
-    route {
+#configure main route table
+resource "aws_default_route_table" "main-rtb" {
+    
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+   
+   route {
         #handle the internet gateway
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id
     }
     
         tags = {
-            Name: "${var.env_prefix}-rtb"
+            Name: "${var.env_prefix}-main-rtb"
         }
-    
 
 }
+
